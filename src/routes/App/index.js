c6789f24e62737037b405e645e6b43ff2b872a84
Jamter Tsai <121978614@qq.com>
2020-01-14 21:22:32 +0800

    fix(BrowserRouter): add basename to fix githubpage black

--- src/routes/App/index.js
+++ src/routes/App/index.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import React, { Suspense, lazy } from 'react'
 
 const Home = lazy(() => import('./children/Home'))
 const Editor = lazy(() => import('./children/Editor'))
 
 const App = () => (
-  <Router>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/editor" component={Editor}/>
       </Switch>
     </Suspense>
-  </Router>
+  </BrowserRouter>
 )
 export default App
